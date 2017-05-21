d76bf56da01ae5be396c42043f3e70c2de1488f4
Sanchensh <xjl1994217@163.com>
2017-05-22 00:37:59 +0800

    javascript
    学习javascript的代码

---
+++ js/Demo_02.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Mr Xu on 2017/5/15.
+ */
+var Olink= document.getElementById("link1");
+function skin1() {
+    Olink.href = "../css/Demo_21.css"
+}
+function skin2() {
+    Olink.href = "../css/Demo_22.css"
+}
